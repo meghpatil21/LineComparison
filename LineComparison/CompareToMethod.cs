355ba16a78c77f1acd9fd75faee0180395eaab68
meghpatil21 <megh.after18@gmail.com>
2022-08-29 13:55:46 +0530

    [megh] Compare2 method

---
+++ LineComparison/CompareToMethod.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class CompareToMethod
+    {
+        public static void CompareLength()
+        {
+            int i;
+            double length1 = 0;
+            double length2 = 0;
+            int count = 0;
+
+            for (i = 1; i <= 2; i++)
+            {
+                count++;
+                Console.WriteLine();
+                Console.WriteLine("For line " + count);
+
+                Console.WriteLine("Enter X Co-ordinates of first point: ");
+                Double x1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter Y Co-ordinates of first point: ");
+                Double y1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter X Co-ordinates of second point: ");
+                Double x2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter X Co-ordinates of second point: ");
+                Double y2 = Convert.ToDouble(Console.ReadLine());
+
+                double x = (x2 - x1) * (x2 - x1);
+                double y = (y2 - y1) * (y2 - y1);
+
+                double lengthOfLine = Math.Sqrt(x + y);
+                //Console.WriteLine("Lengh of line is " + lengthOfLine);
+
+                if (count == 1)
+                {
+                    length1 = lengthOfLine;
+                }
+                else
+                {
+                    length2 = lengthOfLine;
+                }
+
+            }
+            Console.WriteLine();
+            Console.WriteLine(length1.CompareTo(length2));
+
+            //Console.WriteLine("Two lines are equal?\n" + length1.Equals(length2));
+        }
+    }
+}
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Welcome to Line Comparison Computation Program.");
             Console.WriteLine();
 
-            Console.WriteLine("Select option from following: \n1.Lengh of Line \n2.Check equality of two lines");
+            Console.WriteLine("Select option from following: \n1.Lengh of Line \n2.Check equality of two lines \n3.Compare two lines");
             int option=Convert.ToInt32(Console.ReadLine());
 
             switch(option)
@@ -17,6 +17,9 @@
                     break;
                 case 2:
                     EqualsLine.LineEqualCheck();
+                    break;
+                case 3:
+                    CompareToMethod.CompareLength();   
                     break;
                 default:
                     Console.WriteLine("Invalid choice");
